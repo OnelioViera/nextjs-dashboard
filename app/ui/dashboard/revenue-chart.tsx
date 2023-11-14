87555aafaed4c88167053ef0ba5289e74c45d480
Onelio Viera <ojvwebdesign@gmail.com>
2023-11-14 07:26:07 -0500

    update file

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -24,7 +24,7 @@ import { fetchRevenue } from '@/app/lib/data';
   return (
     <div className="w-full md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Recent Revenues
+        Recent Revenue
       </h2>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
